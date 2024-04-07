f186e79603cfa4407a67496b0e52c2a310157410
Andrei Trunkin <inboxavt@gmail.com>
2024-04-07 13:41:54 +0200

    Setting up the environment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
 function App() {
   return (
     <div className="App">
