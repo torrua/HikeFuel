bcd2d77c346df783d7cd55307917af5a4fe07fd4
Andrei Trunkin <inboxavt@gmail.com>
2024-04-09 21:11:23 +0200

    Added SignUpPage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useTranslation } from 'react-i18next';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import SignUpPage from './components/SignUpPage';
 
 const App = () => {
   const { t } = useTranslation();
   return (
-    <div className="App">
-      <div className="m-3 fs-1 fst-italic">{t('pageInfo')}</div>
-    </div>
+    <>
+      <div className="App">
+        <div className="m-3 fs-1 fst-italic">{t('pageInfo')}</div>
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
