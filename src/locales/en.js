22926e5ade44502c0c616c711d29a136f3f891fb
Andrei Trunkin <inboxavt@gmail.com>
2024-04-09 21:09:25 +0200

    Added texts for signup page

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,6 +1,22 @@
+const signUpPage = {
+  header: 'Sign up',
+  userName: 'Username',
+  userNameRequired: 'Specify your username, please',
+  userNameMin: 'From 3 to 20 characters',
+  password: 'Password',
+  passwordRequired: 'Specify your password, please',
+  passwordMin: 'Password must consist of at least {{signs}} characters',
+  confirmPassword: 'Confirm password',
+  confirmPasswordError: 'Confirm your password, please',
+  equalRequired: 'Passwords must match',
+  signUpButton: 'Sign up',
+  formSignUpError: 'Such user already exists',
+};
+
 const en = {
   translation: {
     pageInfo: 'Hike Fuel is on process',
+    signUpPage,
   },
 };
 
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,6 +1,22 @@
+const signUpPage = {
+  header: 'Регистрация',
+  userName: 'Имя пользователя',
+  userNameRequired: 'Пожалуйста, укажите имя пользователя',
+  userNameMin: 'От 3-х до 20-и символов',
+  password: 'Пароль',
+  passwordRequired: 'Пожалуйста, укажите пароль',
+  passwordMin: 'Не менее {{signs}} символов',
+  confirmPassword: 'Подтвердите пароль',
+  confirmPasswordError: 'Пожалуйста, подтвердите пароль',
+  equalRequired: 'Пароли должны совпадать',
+  signUpButton: 'Зарегистрироваться',
+  formSignUpError: 'Такой пользователь уже существует',
+};
+
 const ru = {
   translation: {
     pageInfo: 'Hike Fuel в работе',
+    signUpPage,
   },
 };
 
