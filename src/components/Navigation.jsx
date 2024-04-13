daa668f8a715e2fa5c6d7d57e6d2a81c7f9b450b
Andrei Trunkin <inboxavt@gmail.com>
2024-04-13 23:03:35 +0200

    Updated styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -16,6 +16,8 @@ const Navigation = () => {
     return i18n.changeLanguage('ru');
   };
 
+  // "ms-md-auto" - positioned on the right for medium-width and larger, 
+
     return (
     <>
       <Navbar bg="light" data-bs-theme="light">
@@ -24,12 +26,12 @@ const Navigation = () => {
             HikeFuel
           </Navbar.Brand>
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-md-auto">
             <Nav.Link as={Link} to="/">{t('toMain')}</Nav.Link>
             <Nav.Link as={Link} to="/login">{t('login')}</Nav.Link>
             <Nav.Link as={Link} to="/signup">{t('signup')}</Nav.Link>
           </Nav>
-          <Nav className="btn-group gap-2">
+          <Nav className="gap-1 ms-md-auto">
           <Button className="btn-primary" onClick={setEng} active={i18n.language === 'en' ? true : false}>{t('buttonENG')}</ Button>
           <Button className="btn-primary" onClick={setRu} active={i18n.language === 'ru' ? true : false}>{t('buttonRU')}</ Button>
           </Nav>
