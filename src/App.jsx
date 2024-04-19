b34b613ab489a39e4f46c700444243bcccc0279a
Andrei Trunkin <inboxavt@gmail.com>
2024-04-19 23:32:36 +0200

    Updated route for private page

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-import MainPage from './components/MainPage.jsx'
-import SignUpPage from './components/SignUpPage.jsx';
-import LoginPage from './components/LoginPage.jsx';
-import NotFoundPage from './components/NotFoundPage.jsx';
-import Navigation from './components/Navigation.jsx';
+import MainPage from './pages/MainPage.jsx'
+import UserPage from './pages/UserPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import Navigation from './pages/Navigation.jsx';
 import AuthProvider from './context/AuthProvider.jsx';
 import RequireAuth from './hooks/requireAuth.jsx';
 
@@ -24,9 +25,14 @@ const App = () => {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="*" element={<NotFoundPage />} />
-            <RequireAuth>
-              <Route path="/:userId" element={<></>} />
-            </RequireAuth>
+            <Route 
+              path="/private" 
+              element={(
+                <RequireAuth>
+                  <UserPage />
+                </RequireAuth>
+              )} 
+            />
           </Routes>
         </Router>
       </AuthProvider>
