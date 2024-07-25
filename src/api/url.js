a9ade5d154d50b64020333def2102a2718e53897
Andrei Trunkin <inboxavt@gmail.com>
2024-07-25 23:45:00 +0200

    Created links

--- src/api/url.js
+++ src/api/url.js
@@ -1,19 +1,9 @@
-/* import url from 'url';
-
-const { URL } = url;
-
 const serverUrl = new URL('https://fuel-hike-torrua-06d2d937.koyeb.app');
-
 const serverHref = serverUrl.href;
 
-const postUrl = new URL(serverHref);
-postUrl.pathname = '/auth/register';
-const postHref = postUrl.href;
+const copyServerUrl = new URL(serverHref);
+copyServerUrl.pathname = '/auth/register';
 
-export default postHref; */
-
-const serverHref = 'https://fuel-hike-torrua-06d2d937.koyeb.app';
-
-const postRegisterHref = 'https://fuel-hike-torrua-06d2d937.koyeb.app/auth/register';
+const postRegisterHref = copyServerUrl.href;
 
 export { serverHref, postRegisterHref };
