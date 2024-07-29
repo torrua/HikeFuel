577e21d4de8baba11758b18819b0755ea510dc88
Andrei Trunkin <inboxavt@gmail.com>
2024-07-29 15:56:55 +0200

    Deleted prototype string

--- src/hooks/requireAuth.jsx
+++ src/hooks/requireAuth.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 const RequireAuth = ({ children }) => {
-  const auth = Object.prototype.hasOwnProperty.call(localStorage, 'token');
+  const auth = Object.hasOwnProperty.call(localStorage, 'token');
 
   return auth ? children : <Navigate to="/login" />;
 };
