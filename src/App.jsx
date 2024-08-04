d3fe81a72b600599d8b66eb31faf83d8b5485837
Andrei Trunkin <inboxavt@gmail.com>
2024-08-04 19:43:52 +0200

    Corrected state and state-provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux';
 // import { useTranslation } from 'react-i18next';
 import './App.css';
 
@@ -13,27 +14,30 @@ import NotFoundPage from './pages/NotFoundPage.jsx';
 import Navigation from './pages/Navigation.jsx';
 import AuthProvider from './context/AuthProvider.jsx';
 import RequireAuth from './hooks/requireAuth.jsx';
+import store from './slices/index.js';
 
 const App = () => {
   return (
     <>
       <AuthProvider>
         <Router>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-            <Route 
-              path="/private" 
-              element={(
-                <RequireAuth>
-                  <UserPage />
-                </RequireAuth>
-              )} 
-            />
-          </Routes>
+          <Provider store={store}>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+              <Route 
+                path="/private" 
+                element={(
+                  <RequireAuth>
+                    <UserPage />
+                  </RequireAuth>
+                )} 
+              />
+            </Routes>
+          </Provider>
         </Router>
       </AuthProvider>
     </>
